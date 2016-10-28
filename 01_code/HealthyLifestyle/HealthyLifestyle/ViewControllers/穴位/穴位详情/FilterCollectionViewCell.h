0d65e301c17d349c985b67260f213a3821e4d0ba
白野 <792287662@qq.com>
2016-10-28 17:26:45 +0800

    1

---
+++ 01_code/HealthyLifestyle/HealthyLifestyle/ViewControllers/穴位/穴位详情/FilterCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionViewCell.h
+//  ZhaoQiPei
+//
+//  Created by 找汽配 on 16/9/23.
+//  Copyright © 2016年 XiangYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ 01_code/HealthyLifestyle/HealthyLifestyle/ViewControllers/穴位/穴位详情/FilterCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  FilterCollectionViewCell.m
+//  ZhaoQiPei
+//
+//  Created by 找汽配 on 16/9/23.
+//  Copyright © 2016年 XiangYun. All rights reserved.
+//
+
+#import "FilterCollectionViewCell.h"
+
+@implementation FilterCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
